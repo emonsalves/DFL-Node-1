044e6849bdc7ba9b0de060770f21376b5faf5a30
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-12-06 20:27:59 -0300

    Add files via upload

--- operaciones.js
+++ operaciones.js
@@ -1,18 +1,18 @@
 const fs = require('fs')
 
 const registrar = (nombre, edad, tipo, color, diagnostico) => {
+    const file = fs.readFileSync(`citas.json`, 'utf8')
+    const data = JSON.parse(file)
 
-    const datos = [{
+    const datos = {
         "nombre": nombre,
         "edad": edad,
         "animal": tipo,
         "color": color,
         "enfermedad": diagnostico
-    }]
-
-    fs.writeFileSync(`citas.json`, JSON.stringify(datos))
+    }
+    fs.writeFileSync(`citas.json`, JSON.stringify([...data, datos], null, 4))
     console.log("Cita registrada")
-
 }
 
 const leer = () => {
